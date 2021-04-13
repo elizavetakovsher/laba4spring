ca13e3c84a45eb6e6f95b22c7755342536732fa0
Ilya Khaletski <il.khaletski@gmail.com>
2021-04-13 12:54:01 +0300

    Лаба 4

--- src/main/java/com/epam/web/logic/service/TriangleService.java
+++ src/main/java/com/epam/web/logic/service/TriangleService.java
@@ -5,18 +5,27 @@ import com.epam.web.entity.TriangleParameters;
 import com.epam.web.exceptions.TriangleCalculatorException;
 import com.epam.web.logic.calculator.Calculator;
 import com.epam.web.logic.calculator.TriangleCalculator;
+import com.epam.web.logic.counter.AppealsCounter;
 import com.epam.web.logic.hash.TriangleHash;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@AllArgsConstructor
 @org.springframework.stereotype.Service
 public class TriangleService implements Service<TriangleParameters, Triangle> {
 
-    @Autowired
-    private TriangleHash hash;
+
+    private final TriangleHash hash;
+    private final AppealsCounter appealsCounter;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(5);
 
 
     @Override
     public TriangleParameters doService(Triangle entity) throws TriangleCalculatorException {
+        executorService.submit(appealsCounter);
         if(hash.isAlreadyHashed(entity)){
             return hash.getParameters(entity);
         }
